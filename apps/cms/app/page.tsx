ecdd8e8f9d0357537a20a50d65ccc6717d9d4c34
huytv04883 <huytv04883@gmail.com>
2026-03-03 15:22:47 +0700

    update cms

--- apps/cms/app/page.tsx
+++ apps/cms/app/page.tsx
@@ -1,32 +1,5 @@
-import Image, { type ImageProps } from "next/image";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default function Home() {
   return (
-    <ThemeImage
-      className={styles.logo}
-      srcLight="turborepo-dark.svg"
-      srcDark="turborepo-light.svg"
-      alt="Turborepo logo"
-      width={180}
-      height={38}
-      priority
-    />
+    <h1>This Page is Cms</h1>
   );
 }
